f023008d5acdbde99eeca8f23ef57b6947b06e4d
Karina <xmoonan@gmail.com>
2020-05-11 18:54:50 +0600

    Move the api key to keys.properties

--- app/src/main/java/jp/neechan/akari/dictionary/common/network/AuthorizationInterceptor.kt
+++ app/src/main/java/jp/neechan/akari/dictionary/common/network/AuthorizationInterceptor.kt
@@ -1,19 +1,20 @@
 package jp.neechan.akari.dictionary.common.network
 
+import jp.neechan.akari.dictionary.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class AuthorizationInterceptor : Interceptor {
 
     companion object {
-        private const val AUTHORIZATION_TOKEN_KEY = "X-Mashape-Key"
-        private const val AUTHORIZATION_TOKEN_VALUE = "8395b7db36mshce928fb38d86eb8p1e163bjsn8070f6b090a6"
+        private const val API_KEY_HEADER = "X-Mashape-Key"
+        private const val API_KEY = BuildConfig.API_KEY
     }
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
         val authenticatedRequest = request.newBuilder()
-                                          .header(AUTHORIZATION_TOKEN_KEY, AUTHORIZATION_TOKEN_VALUE)
+                                          .header(API_KEY_HEADER, API_KEY)
                                           .build()
         return chain.proceed(authenticatedRequest)
     }
