6c02597faf8135bad00549e800fb1cc5ed1fa4a5
Karina <xmoonan@gmail.com>
2020-05-28 10:35:05 +0600

    RetrofitResultWrapper -> do not switch CoroutineContext, because the context should be defined in a call place

--- app/src/main/java/jp/neechan/akari/dictionary/base/data/interface_adapters/ResultWrapper.kt
+++ app/src/main/java/jp/neechan/akari/dictionary/base/data/interface_adapters/ResultWrapper.kt
@@ -1,11 +1,8 @@
 package jp.neechan.akari.dictionary.base.data.interface_adapters
 
-import jp.neechan.akari.dictionary.base.domain.entities.mappers.ModelMapper
 import jp.neechan.akari.dictionary.base.domain.entities.Result
 
 interface ResultWrapper {
 
     suspend fun <T> wrapWithResult(block: suspend () -> T): Result<T>
-
-    suspend fun <T, R> wrapWithResult(block: suspend () -> T, mapper: ModelMapper<R, T>): Result<R>
 }
