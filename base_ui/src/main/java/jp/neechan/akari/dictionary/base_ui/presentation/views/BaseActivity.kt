47509cad188598e25c6ee7f29200f084cd4a2bb3
Karina <xmoonan@gmail.com>
2020-06-08 17:44:34 +0600

    base_ui files

---
+++ base_ui/src/main/java/jp/neechan/akari/dictionary/base_ui/presentation/views/BaseActivity.kt
@@ -0,0 +1,24 @@
+package jp.neechan.akari.dictionary.base_ui.presentation.views
+
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    protected fun setupBackButton() {
+        supportActionBar?.apply {
+            setDisplayShowHomeEnabled(true)
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == android.R.id.home) {
+            onBackPressed()
+            true
+
+        } else {
+            super.onOptionsItemSelected(item)
+        }
+    }
+}
---
+++ base_ui/src/main/java/jp/neechan/akari/dictionary/base_ui/presentation/views/BaseFragment.kt
@@ -0,0 +1,5 @@
+package jp.neechan.akari.dictionary.base_ui.presentation.views
+
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment : Fragment()
